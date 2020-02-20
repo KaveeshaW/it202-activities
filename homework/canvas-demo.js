a32ccc4aafbfdb9cabce49b436583f265168db71
Kaveesha Weerasiri <kweera2@uic.edu>
2020-02-20 22:21:02 +0000

    should be done with homework_6.3

--- homework/canvas-demo.js
+++ homework/canvas-demo.js
@@ -1,45 +1,52 @@
-// canvas-demo.js
-
 const demoCanvas = document.getElementById('canvas-demo').getContext('2d');
 
-window.onload = function() {// make sure to use onload
+const notContextCanvas = document.getElementById('canvas-demo');
 
-// Calling this isn't necessary, but a good practice.
-demoCanvas.beginPath();
+function drawFirstLetter() {
+  demoCanvas.strokeStyle = "blue";
+  demoCanvas.lineWidth = 10;
 
-/* Draw horizontal subpaths (shapes) in one path. */
+  demoCanvas.moveTo(10, 10);
+  demoCanvas.lineTo(10, 110);
+  demoCanvas.moveTo(10, 60);
+  demoCanvas.lineTo(50, 10);
+  demoCanvas.moveTo(10, 60);
+  demoCanvas.lineTo(50, 110);
 
-// Draw a pattern of vertically stack horizontal
-// lines.
-demoCanvas.moveTo(10, 10);// start at (10,10)
-demoCanvas.lineTo(110, 10);
-demoCanvas.moveTo(10, 20);// 10 pts below
-demoCanvas.lineTo(180, 20);
-demoCanvas.moveTo(10, 30);
-demoCanvas.lineTo(150, 30);
-demoCanvas.moveTo(10, 40);
-demoCanvas.lineTo(160, 40);
-demoCanvas.moveTo(10, 50);
-demoCanvas.lineTo(130, 50);
-
-// try removing this moveTo, the quad-curve will then
-// start from from (130, 50), due to the lineTo.
-demoCanvas.moveTo(10, 100);// quad-curve starts from here
-demoCanvas.quadraticCurveTo(110, 55, 210, 100);// curve upward
-demoCanvas.moveTo(10, 100);// back here, let's draw one below
-demoCanvas.quadraticCurveTo(110, 145, 210, 100);// curve below
-// that forms the eye outline
-
-demoCanvas.moveTo(132.5, 100);// remove this, a horizontal line will be
-// drawn from (210, 100) to (132.5, 100) because arc() connects the last
-// point to the start of the arc.
-
-demoCanvas.arc(110, 100, 22.5, 0, 2*Math.PI, false);// pupil (circle)
-
-/* We'll talk about this shortly */
-demoCanvas.stroke();// draws (by outlining our shapes in the path)
-
-demoCanvas.closePath();// this is required if you want to draw
-// in a separate path later
-    
+  demoCanvas.stroke();// draws (by outlining our shapes in the path)
 }
+
+function drawShape() {
+  demoCanvas.fillStyle = "red";
+  demoCanvas.lineWidth = 1;
+  demoCanvas.moveTo(90, 200);
+  demoCanvas.arc(90, 200, 30, 0, 360, false);
+  demoCanvas.fill();
+}
+
+window.onload = function() {
+  
+  //fills the canvas with a purple background
+  demoCanvas.beginPath();
+  demoCanvas.fillStyle = "purple";
+  demoCanvas.fillRect(0, 0, notContextCanvas.width, notContextCanvas.height);
+  demoCanvas.closePath();
+
+  //draws my first initial (which is a k)
+  demoCanvas.beginPath();
+  drawFirstLetter();
+  demoCanvas.closePath();
+
+  //draws a circle
+  demoCanvas.beginPath();
+  drawShape();
+  demoCanvas.closePath();
+
+  demoCanvas.beginPath();
+  demoCanvas.fillStyle = "pink";
+  demoCanvas.font = "30px Times New Roman";
+  demoCanvas.fillText("660036548", 30, 300, 300);
+  demoCanvas.closePath();
+  
+}
+
